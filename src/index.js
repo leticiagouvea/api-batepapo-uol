25e79fcf3ba9a11b81ca5865d0eef516575b3062
leticiagouvea <letfgouvea@gmail.com>
2022-11-13 21:21:17 -0300

    POST messages

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { text } from 'express';
 import cors from 'cors';
 import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
@@ -85,6 +85,51 @@ app.get("/participants", async (req, res) => {
         res.sendStatus(500);
     }
 });
+
+// MESSAGES ROUTES
+
+const schemaMessages = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.valid("message", "private_message")
+});
+
+app.post("/messages", async (req, res) => {
+    const { user } = req.headers;
+    const { to, text, type } = req.body;
+    
+    const validation = schemaMessages.validate(req.body, { abortEarly: false });
+
+    if (validation.error) {
+        const error = validation.error.details.map(detail => detail.message);
+        res.status(422).send(error);
+        return;
+    }
+
+    try {
+        const existingUser = await collectionParticipants.findOne({ name: user });
+
+        if(!existingUser) {
+            return res.status(422).send("Usuário inexistente");
+        }
+
+        await collectionMessages.insertOne({
+            from: user,
+            to,
+            text,
+            type,
+            time: date
+        });
+
+        res.sendStatus(201);
+
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
+
+
 
 app.listen(5000, () => {
     console.log("App is running in port: 5000");
